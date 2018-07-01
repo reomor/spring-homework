10630c7bc18abef8381687e52aa3aa4277536dff
Reomor <reomor@gmail.com>
2018-07-01 14:21:42 +0300

    refactoring

--- task02/src/main/java/task02/service/QuestionService.java
+++ task02/src/main/java/task02/service/QuestionService.java
@@ -1,9 +1,12 @@
 package task02.service;
 
 import task02.model.Question;
-import task02.utils.Utils;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class QuestionService {
@@ -15,6 +18,36 @@ public class QuestionService {
     }
 
     public List<Question> loadQuestion() throws IOException {
-        return Utils.readCSVInQuestions(questionFileName);
+        return readCSVInQuestions(questionFileName);
+    }
+
+    private List<Question> readCSVInQuestions(String fileName) throws IOException {
+        List<Question> questions = new ArrayList<>();
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream(fileName)))) {
+            String line = null;
+            while ((line = getNextString(bufferedReader)) != null) {
+                String[] questionString = line.split(",");
+                Integer rightAnswer = Integer.parseInt(getNextString(bufferedReader));
+                Question question = new Question(
+                        questionString[0],
+                        Arrays.asList(Arrays.copyOfRange(questionString, 1, questionString.length)),
+                        rightAnswer);
+                questions.add(question);
+            }
+        } catch (IOException e) {
+            throw new IOException("Error during read \"" + fileName + "\".");
+        }
+        return questions;
+    }
+
+    private static String getNextString(BufferedReader bufferedReader) throws IOException {
+        String inputString = bufferedReader.readLine();
+        if (inputString == null) {
+            return null;
+        }
+        while (inputString.length() == 0 || inputString.startsWith("#")) {
+            inputString = bufferedReader.readLine();
+        }
+        return inputString;
     }
 }
