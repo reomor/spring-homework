30776e1028bacc24117b6d0050a6d4a624597a2b
Reomor <reomor@gmail.com>
2018-07-08 19:46:20 +0300

    models and services

---
+++ task04/src/main/java/task04/model/User.java
@@ -0,0 +1,19 @@
+package task04.model;
+
+public class User {
+    private final String sername;
+    private final String name;
+
+    public User(String name, String sername) {
+        this.sername = sername;
+        this.name = name;
+    }
+
+    public String getSername() {
+        return sername;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
