04ad15fd26ab14884aa07de86e2cefe4bea5cf0e
Reomor <reomor@gmail.com>
2018-07-10 23:48:09 +0300

    application settings

--- task04/src/main/java/task04/setting/ApplicationSettings.java
+++ task04/src/main/java/task04/setting/ApplicationSettings.java
@@ -3,6 +3,8 @@ package task04.setting;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 @Component
@@ -12,6 +14,37 @@ public class ApplicationSettings {
     private String fileQuestion;
     private Locale locale;
     private String loadType;
+    private List<ApplicationSettings.Question> questions = new ArrayList<>();
+
+    public static class Question {
+
+        private String questionString;
+        private Integer rightAnswer;
+
+        public String getQuestionString() {
+            return questionString;
+        }
+
+        public void setQuestionString(String questionString) {
+            this.questionString = questionString;
+        }
+
+        public Integer getRightAnswer() {
+            return rightAnswer;
+        }
+
+        public void setRightAnswer(Integer rightAnswer) {
+            this.rightAnswer = rightAnswer;
+        }
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(List<Question> questions) {
+        this.questions = questions;
+    }
 
     public String getFileQuestion() {
         return fileQuestion;
--- task04/src/test/java/task04/setting/ApplicationSettingsTest.java
+++ task04/src/test/java/task04/setting/ApplicationSettingsTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
 import java.util.Locale;
 
 import static org.junit.Assert.*;
@@ -13,7 +14,15 @@ import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationSettingsTest {
-
+/*
+    @Configuration
+    static class ApplicationSettingsContextConfiguration {
+        @Bean
+        public ApplicationSettings applicationSettings() {
+            return new ApplicationSettings();
+        }
+    }
+//*/
     @Autowired
     private ApplicationSettings applicationSettings;
 
@@ -23,5 +32,7 @@ public class ApplicationSettingsTest {
         assertEquals(Locale.ENGLISH, locale);
         String fileQuestion = applicationSettings.getFileQuestion();
         assertEquals("questions.csv", fileQuestion);
+        List<ApplicationSettings.Question> questions = applicationSettings.getQuestions();
+        assertEquals(5, questions.size());
     }
 }
