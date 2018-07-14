c2e8f8e53912b8df92a22c5e38ce78ca46ddb2f5
Reomor <reomor@gmail.com>
2018-07-14 17:37:44 +0300

    dependencies and main

--- task06/src/main/java/task06/Main.java
+++ task06/src/main/java/task06/Main.java
@@ -1,9 +1,13 @@
 package task06;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
+        ApplicationContext context = SpringApplication.run(Main.class);
     }
 }
