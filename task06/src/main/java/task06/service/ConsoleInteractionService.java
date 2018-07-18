769d7708590c9587da2bba86ded61490e4ce938d
reomor <reomor@gmail.com>
2018-07-18 17:19:30 +0300

    delegate commands to shell, console functions to service

--- task06/src/main/java/task06/service/ConsoleInteractionService.java
+++ task06/src/main/java/task06/service/ConsoleInteractionService.java
@@ -1,7 +1,41 @@
 package task06.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import task06.dao.AuthorDao;
+import task06.dao.BookDao;
+import task06.dao.GenreDao;
+
+import java.util.List;
 
 @Service
 public class ConsoleInteractionService {
+    private final BookDao bookDao;
+    private final AuthorDao authorDao;
+    private final GenreDao genreDao;
+
+    @Autowired
+    public ConsoleInteractionService(BookDao bookDao, AuthorDao authorDao, GenreDao genreDao) {
+        this.bookDao = bookDao;
+        this.authorDao = authorDao;
+        this.genreDao = genreDao;
+    }
+
+    public void getall(String dao) {
+        if ("book".equals(dao)) {
+            printList(bookDao.getAll());
+        } else if ("author".equals(dao)) {
+            printList(authorDao.getAll());
+        } else if ("genre".equals(dao)) {
+            printList(genreDao.getAll());
+        }
+    }
+
+    private <T> void printList(List<T> list) {
+        System.out.println("=== list begin ===");
+        for (T element : list) {
+            System.out.println(element);
+        }
+        System.out.println("=== list end ===");
+    }
 }
--- task06/src/main/java/task06/shell/Commands.java
+++ task06/src/main/java/task06/shell/Commands.java
@@ -6,29 +6,20 @@ import org.springframework.shell.standard.ShellMethod;
 import task06.dao.AuthorDao;
 import task06.dao.BookDao;
 import task06.dao.GenreDao;
+import task06.service.ConsoleInteractionService;
 
 @ShellComponent
 public class Commands {
 
-    private final BookDao bookDao;
-    private final AuthorDao authorDao;
-    private final GenreDao genreDao;
+    private final ConsoleInteractionService consoleInteractionService;
 
     @Autowired
-    public Commands(BookDao bookDao, AuthorDao authorDao, GenreDao genreDao) {
-        this.bookDao = bookDao;
-        this.authorDao = authorDao;
-        this.genreDao = genreDao;
+    public Commands(ConsoleInteractionService consoleInteractionService) {
+        this.consoleInteractionService = consoleInteractionService;
     }
 
     @ShellMethod("Get all values")
-    public void getall(String dao) {
-        if ("book".equals(dao)) {
-            System.out.println(bookDao.getAll());
-        } else if ("author".equals(dao)) {
-            System.out.println(authorDao.getAll());
-        } else if ("genre".equals(dao)) {
-            System.out.println(genreDao.getAll());
-        }
+    public void getall(String obejctName) {
+        consoleInteractionService.getall(obejctName);
     }
 }
