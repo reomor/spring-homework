e16f9b2b6f43feb83baeefe893d0c6efe940e3ad
reomor <reomor@gmail.com>
2018-08-17 16:44:39 +0300

    book controller in progress

--- task10/src/main/java/task10/rest/BookController.java
+++ task10/src/main/java/task10/rest/BookController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import task10.domain.Book;
 import task10.repository.BookRepository;
 
@@ -23,11 +26,37 @@ public class BookController {
 
     @GetMapping("/book")
     public String listPage(Model model) {
-        log.info("Request all authors");
+        log.info("Request all books");
         List<Book> books = bookRepository.findAll();
         model.addAttribute("books", books);
         return "book/list";
     }
-    // https://www.thymeleaf.org/doc/articles/layouts.html
-    // https://github.com/thymeleaf/thymeleafexamples-layouts/tree/master/src/main/java/thymeleafexamples/layouts
+
+    @GetMapping("/book/view")
+    public String editPage(
+            @RequestParam(value = "id", required = false) String id,
+            @RequestParam(value = "edit", required = false) Boolean edit,
+            Model model) {
+        log.info("Edit book with id " + id);
+        Book book = bookRepository.findById(id).orElseThrow(RuntimeException::new);
+        String action = "/book";
+        if (id == null) {
+
+        } else {
+            action += "?id=" + id;
+        }
+        model.addAttribute("action", action);
+        if (edit != null) {
+            model.addAttribute("edit", edit);
+        }
+        model.addAttribute("book", book);
+        return "book/view";
+    }
+
+    @PostMapping("/book")
+    public String postPage(@ModelAttribute Book book, Model model) {
+        log.info("Got book to update " + book);
+        //bookRepository.save(book);
+        return "redirect:/book";
+    }
 }
