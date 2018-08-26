ac4569abf3eecb6c03f15da048e30aa851fb11b1
Reomor <reomor@gmail.com>
2018-08-26 19:43:13 +0300

    cleanup

--- task11/src/main/java/task11/rest/GeneralController.java
+++ task11/src/main/java/task11/rest/GeneralController.java
@@ -5,11 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import task11.domain.Book;
 import task11.repository.AuthorRepository;
 import task11.repository.BookRepository;
-
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -39,6 +36,6 @@ public class GeneralController {
     @GetMapping("/books")
     public String listBooksPage(Model model) {
         log.info("Request book list");
-        return "book/list_bk";
+        return "book/list";
     }
 }
