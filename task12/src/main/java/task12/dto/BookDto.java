a5f04f6a6633eb5f6782381db59bcfc1e76384ad
Reomor <reomor@gmail.com>
2018-08-31 23:06:38 +0300

    task12 base on task11

---
+++ task12/src/main/java/task12/dto/BookDto.java
@@ -0,0 +1,21 @@
+package task11.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+import task11.domain.Author;
+import task11.domain.Book;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Data
+@ToString
+@AllArgsConstructor
+public class BookDto {
+    @ApiModelProperty(notes = "Book")
+    private Book book;
+    @ApiModelProperty(notes = "List of authors")
+    private List<Author> authorList;
+    @ApiModelProperty(notes = "List of author ids", example = "[\"5b77ff92f489de2738fa6c07\", \"5b77ff92f489de2738fa6ad3\"]")
+    private List<String> authorIds;
+}
