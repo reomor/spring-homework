49d9e0dcede34025f3f1f9eba9085971d72e50dd
Reomor <reomor@gmail.com>
2018-10-03 23:32:22 +0300

    simple channel

--- task16/src/main/java/task16/Main.java
+++ task16/src/main/java/task16/Main.java
@@ -1,17 +1,33 @@
 package task16;
 
-import org.h2.tools.Console;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+import task16.config.Config;
 
 import java.sql.SQLException;
 
 @SpringBootApplication
-public class Main {
+@EnableIntegration
+@IntegrationComponentScan
+public class Main implements CommandLineRunner {
+
+    private final Config.InputGateway inputGateway;
+
+    @Autowired
+    public Main(Config.InputGateway inputGateway) {
+        this.inputGateway = inputGateway;
+    }
 
     public static void main(String[] args) throws SQLException {
-        ApplicationContext context = SpringApplication.run(Main.class);
-        Console.main(args);
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        inputGateway.run("Test string");
     }
 }
--- task16/src/main/java/task16/config/Config.java
+++ task16/src/main/java/task16/config/Config.java
@@ -1,7 +1,23 @@
 package task16.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
 
 @Configuration
 public class Config {
+
+    @Bean
+    public IntegrationFlow sampleFlow() {
+        return IntegrationFlows.from("in").log().get();
+    }
+
+    @MessagingGateway
+    public interface InputGateway {
+        @Gateway(requestChannel = "in")
+        void run(String payload);
+    }
 }
