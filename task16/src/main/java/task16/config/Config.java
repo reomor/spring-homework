ff4b4a5d50ffaa12bdcf9383e869014ca8ba3bd8
Reomor <reomor@gmail.com>
2018-10-06 14:00:12 +0300

    message handling

--- task16/src/main/java/task16/config/Config.java
+++ task16/src/main/java/task16/config/Config.java
@@ -4,20 +4,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.dsl.DelayerEndpointSpec;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.handler.LoggingHandler;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @Configuration
 public class Config {
-
     @Bean
     public IntegrationFlow sampleFlow() {
-        return IntegrationFlows.from("in").log().get();
+        return IntegrationFlows.from("in1").log().get();
+    }
+
+    @Bean
+    public IntegrationFlow complexFlow() {
+        return f -> f.channel("in2").log().handle(message -> {
+            Message<String> newM = MessageBuilder.createMessage("Test", message.getHeaders());
+        }).delay("normalMessage", (DelayerEndpointSpec e) -> e.defaultDelay(2000)).channel("in1");
     }
 
     @MessagingGateway
     public interface InputGateway {
-        @Gateway(requestChannel = "in")
+        @Gateway(requestChannel = "in2")
         void run(String payload);
     }
 }
